57298fac3c0c49df826814eda819c3ae212e867b
Leochukwu <leocchukwu@gmail.com>
2024-05-03 13:08:48 +0100

    Homepage added

---
+++ frontend/src/Components/Admin_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Admin_view.css"
+
+const Admin_page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Admin_page
---
+++ frontend/src/Components/Homepage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Homepage.css"
+
+const Homepage = () => {
+  return (
+    <div className='frame'>
+        
+      <div className='login_frame' id='scroll-container'>
+      <h1 id='scroll-text'>ZIDIO FEEDBACK SYSTEM</h1>
+      <h3>PLEASE SELECT APPROPRIATE USER</h3>
+      <div className="frames">
+      <div className="admin_logo">
+            <img className='user_img' src="../images/admin3.png" alt="" />
+            <button>ADMINISTRATOR</button>
+        </div>
+        <div className="admin_logo">
+            <img className='admin_img' src="../images/user.png" alt="" />
+            <button>USER</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
